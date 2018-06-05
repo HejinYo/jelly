c5607e704a974afef7885c400ec076bf2b058a48
HejinYo <hejinyo@gmail.com>
2018-06-05 23:26:55 +0800

    重构redis相关

--- src/main/java/cn/hejinyo/jelly/common/utils/RedisUtils.java
+++ src/main/java/cn/hejinyo/jelly/common/utils/RedisUtils.java
@@ -37,48 +37,10 @@ public class RedisUtils {
      * 不设置过期时长
      */
     private final static long NOT_EXPIRE = -1;
-
-
-    public void set(String key, Object value) {
-        // Key不在，超时返回-2
-        Long expire = redisTemplate.getExpire(key);
-        set(key, value, expire != -2 ? expire : DEFAULT_EXPIRE);
-    }
-
-    public void set(String key, Object value, long expire) {
-        valueOperations.set(key, toJson(value));
-        if (expire != NOT_EXPIRE) {
-            redisTemplate.expire(key, expire, TimeUnit.SECONDS);
-        }
-    }
-
-    public <T> T get(String key, Class<T> clazz, long expire) {
-        String value = valueOperations.get(key);
-        if (expire != NOT_EXPIRE) {
-            redisTemplate.expire(key, expire, TimeUnit.SECONDS);
-        }
-        return value == null ? null : JsonUtil.fromJson(value, clazz);
-    }
-
-    public <T> T get(String key, Class<T> clazz) {
-        return get(key, clazz, NOT_EXPIRE);
-    }
-
-    public String get(String key, long expire) {
-        String value = valueOperations.get(key);
-        if (expire != NOT_EXPIRE) {
-            redisTemplate.expire(key, expire, TimeUnit.SECONDS);
-        }
-        return value;
-    }
-
-    public String get(String key) {
-        return get(key, NOT_EXPIRE);
-    }
-
-    public void delete(String key) {
-        redisTemplate.delete(key);
-    }
+    /**
+     * key不存在返回的过期時常
+     */
+    private final static long NO_KEY_EXPIRE = -2;
 
     /**
      * Object转成JSON数据
@@ -92,7 +54,14 @@ public class RedisUtils {
     }
 
     /**
-     * 清除key
+     * 删除一个key
+     */
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+
+    /**
+     * 清除所有前缀为key
      */
     public String cleanKey(String key) {
         Set<String> keys = redisTemplate.keys(key);
@@ -101,4 +70,148 @@ public class RedisUtils {
         }
         return keys.toString();
     }
+
+    /**
+     * SET key value [EX seconds] [PX milliseconds] [NX|XX] 设置一个key的value值,有效期永久
+     * EX seconds – 设置键key的过期时间，单位时秒
+     * PX milliseconds – 设置键key的过期时间，单位时毫秒
+     * NX – 只有键key不存在的时候才会设置key的值
+     * XX – 只有键key存在的时候才会设置key的值
+     */
+    public void set(String key, Object value) {
+        valueOperations.set(key, toJson(value));
+    }
+
+    /**
+     * SETNX key value 设置的一个关键的价值，只有当该键不存在
+     */
+    public void setnx(String key, Object value) {
+        valueOperations.setIfAbsent(key, toJson(value));
+    }
+
+    /**
+     * SETEX key seconds value 设置key-value并设置过期时间（单位：秒）
+     */
+    public void setex(String key, Object value, long expire) {
+        valueOperations.set(key, toJson(value), expire, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 设置 key value,不改变原来有效期
+     */
+    public void setHold(String key, Object value) {
+        // Key不在，超时返回-2
+        Long expire = redisTemplate.getExpire(key);
+        valueOperations.set(key, toJson(value), expire != NO_KEY_EXPIRE ? expire : DEFAULT_EXPIRE, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 获取一个值
+     */
+    public String get(String key) {
+        return get(key, NOT_EXPIRE);
+    }
+
+    /**
+     * 获取一个值，并设置有效时间
+     */
+    public String get(String key, long expire) {
+        String value = valueOperations.get(key);
+        if (expire != NOT_EXPIRE) {
+            redisTemplate.expire(key, expire, TimeUnit.SECONDS);
+        }
+        return value;
+    }
+
+    /**
+     * 获取一个值，并转换成指定对象
+     */
+    public <T> T get(String key, Class<T> clazz) {
+        return get(key, clazz, NOT_EXPIRE);
+    }
+
+    /**
+     * 获取一个值，并转换成指定对象，并设置有效时间
+     */
+    public <T> T get(String key, Class<T> clazz, long expire) {
+        String value = valueOperations.get(key);
+        if (expire != NOT_EXPIRE) {
+            redisTemplate.expire(key, expire, TimeUnit.SECONDS);
+        }
+        return value == null ? null : JsonUtil.fromJson(value, clazz);
+    }
+
+    /**
+     * APPEND key value
+     * 追加一个值到key上
+     */
+    public Integer append(String key, Object value) {
+        return valueOperations.append(key, toJson(value));
+    }
+
+    /**
+     * DECR key 整数原子减1
+     */
+    public Long decr(String key) {
+        return valueOperations.increment(key, -1);
+    }
+
+    /**
+     * DECR key 整数原子减指定整数
+     */
+    public Long decrby(String key, Long decrement) {
+        return valueOperations.increment(key, -decrement);
+    }
+
+    /**
+     * INCR key 执行原子加1操作
+     */
+    public Long incr(String key) {
+        return valueOperations.increment(key, 1);
+    }
+
+    /**
+     * INCRBY key 整数原子加指定整数
+     */
+    public Long incrby(String key, Long decrement) {
+        return valueOperations.increment(key, decrement);
+    }
+
+    /**
+     * GETSET key value 设置一个key的value，并获取设置前的值
+     */
+    public String getAndSet(String key, Object value) {
+        return valueOperations.getAndSet(key, toJson(value));
+    }
+
+    /**
+     * BLPOP key [key ...] timeout
+     * 删除，并获得该列表中的第一元素，或阻塞，直到有一个可用
+     */
+    public Object blpop(String key, long timeout) {
+        return listOperations.leftPop(key, timeout, TimeUnit.SECONDS);
+    }
+
+    /**
+     * BRPOP key [key ...] timeout 删除，并获得该列表中的最后一个元素，或阻塞，直到有一个可用
+     */
+    public Object brpop(String key, long timeout) {
+        return listOperations.rightPop(key, timeout, TimeUnit.SECONDS);
+    }
+
+    /**
+     * BRPOPLPUSH source destination timeout 弹出一个列表的值，将它推到另一个列表，并返回它;或阻塞，直到有一个可用
+     */
+    public Object brpoplpush(String source, String destination, long timeout) {
+        if (timeout == 0) {
+            // timeout 为 0 能用于无限期阻塞客户端。
+            return listOperations.rightPopAndLeftPush(source, destination);
+        }
+        return listOperations.rightPopAndLeftPush(source, destination, timeout, TimeUnit.SECONDS);
+    }
+
+    public Object lpop(String key) {
+        return listOperations.leftPop(key);
+    }
+
 }
--- src/main/java/cn/hejinyo/jelly/config/RedisConfig.java
+++ src/main/java/cn/hejinyo/jelly/config/RedisConfig.java
@@ -1,25 +1,77 @@
 package cn.hejinyo.jelly.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
- * Redis配置
+ * Redis配置，开启注解式缓存
+ *
+ * @author : HejinYo   hejinyo@gmail.com
+ * @date : 2017/8/17 23:22
  */
 @Configuration
+@EnableCaching
 public class RedisConfig {
+    /**
+     * 默认缓存时长
+     */
+    private static final long DEFAULTEXPIRETIME = 600;
+
+    /**
+     * 缓存管理器
+     *
+     * @ Cacheable(value = "user", key = "#root.targetClass.getCanonicalName()", unless = "#result eq null")
+     * @ CachePut
+     * @ CacheEvict
+     */
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        /*Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+    public CacheManager cacheManager(RedisTemplate jsonRedisTemplate) {
+        RedisCacheManager cacheManager = new RedisCacheManager(jsonRedisTemplate);
+        //设置缓存过期时间
+        cacheManager.setDefaultExpiration(DEFAULTEXPIRETIME);
+        cacheManager.setUsePrefix(true);
+        cacheManager.setCachePrefix(cacheName -> ("cache:" + cacheName).getBytes());
+        return cacheManager;
+    }
+
+    /**
+     * jsonRedisTemplate
+     * Jackson2JsonRedisSerializer来反序列化value值
+     * StringRedisSerializer来序列化的key值
+     */
+    @Bean("jsonRedisTemplate")
+    public RedisTemplate<Object, Object> jsonRedisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        //使用Jackson2JsonRedisSerializer来反序列化redis的value值
+        Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer<>(Object.class);
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        jackson2JsonRedisSerializer.setObjectMapper(om);*/
+        serializer.setObjectMapper(om);
+        template.setValueSerializer(serializer);
+        //使用StringRedisSerializer来序列化的key值
+        template.setKeySerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
 
+    /**
+     * StringRedisSerializer来序列化和反序列化的RedisTemplate
+     */
+    @Bean("redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
         redisTemplate.setHashValueSerializer(new StringRedisSerializer());
@@ -28,10 +80,6 @@ public class RedisConfig {
         return redisTemplate;
     }
 
-    @Bean
-    public HashOperations<String, String, Object> hashOperations(RedisTemplate<String, Object> redisTemplate) {
-        return redisTemplate.opsForHash();
-    }
 
     @Bean
     public ValueOperations<String, String> valueOperations(RedisTemplate<String, String> redisTemplate) {
@@ -51,5 +99,10 @@ public class RedisConfig {
     @Bean
     public ZSetOperations<String, Object> zSetOperations(RedisTemplate<String, Object> redisTemplate) {
         return redisTemplate.opsForZSet();
+    }
+
+    @Bean
+    public HashOperations<String, String, Object> hashOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForHash();
     }
 }
