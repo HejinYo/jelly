4c4a4227313bc5af44dcf0f7b1e7d8cc6c72236d
HejinYo <hejinyo@gmail.com>
2018-06-06 23:17:09 +0800

    update : redis工具欄

--- src/main/java/cn/hejinyo/jelly/common/utils/RedisUtils.java
+++ src/main/java/cn/hejinyo/jelly/common/utils/RedisUtils.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.*;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -21,9 +23,9 @@ public class RedisUtils {
     @Autowired
     private ValueOperations<String, String> valueOperations;
     @Autowired
-    private HashOperations<String, String, Object> hashOperations;
-    @Autowired
     private ListOperations<String, Object> listOperations;
+    @Autowired
+    private HashOperations<String, String, Object> hashOperations;
     @Autowired
     private SetOperations<String, Object> setOperations;
     @Autowired
@@ -210,8 +212,192 @@ public class RedisUtils {
         return listOperations.rightPopAndLeftPush(source, destination, timeout, TimeUnit.SECONDS);
     }
 
+
+    /**
+     * RPOPLPUSH source destination 删除列表中的最后一个元素，将其追加到另一个列表
+     */
+    public Object rpoplpush(String sourceKey, String destinationKey) {
+        return listOperations.rightPopAndLeftPush(sourceKey, destinationKey);
+    }
+
+    /**
+     * LINDEX key index 获取一个元素，通过其索引列表
+     */
+    public Object lindex(String key, long index) {
+        return listOperations.index(key, index);
+    }
+
+    /**
+     * LLEN key 获得队列(List)的长度
+     */
+    public Object llen(String key) {
+        return listOperations.size(key);
+    }
+
+    /**
+     * LPOP key 从队列的左边出队一个元素
+     */
     public Object lpop(String key) {
         return listOperations.leftPop(key);
     }
+
+    /**
+     * LPUSH key value [value ...] 从队列的左边入队一个元素
+     */
+    public Long lpush(String key, Object value) {
+        return listOperations.leftPush(key, value);
+    }
+
+    /**
+     * LPUSH key value [value ...] 从队列的左边入队多个元素
+     */
+    public Long lpush(String key, Object... values) {
+        return listOperations.leftPushAll(key, values);
+    }
+
+    /**
+     * LPUSH key value [value ...] 从队列的左边入队一个集合
+     */
+    public Long lpush(String key, Collection<Object> values) {
+        return listOperations.leftPushAll(key, values);
+    }
+
+    /**
+     * LPUSHX key value 当队列存在时，从队到左边入队一个元素
+     */
+    public Long lpushx(String key, Object value) {
+        return listOperations.leftPushIfPresent(key, value);
+    }
+
+    /**
+     * LRANGE key start stop 从列表中获取指定返回的元素
+     * list的第一个元素下标是0（list的表头），第二个元素下标是1，以此类推。
+     * 偏移量也可以是负数，表示偏移量是从list尾部开始计数。 例如， -1 表示列表的最后一个元素，-2 是倒数第二个，以此类推。
+     */
+    public List<Object> lrange(String key, long start, long end) {
+        return listOperations.range(key, start, end);
+    }
+
+    /**
+     * LREM key count value 从列表中删除元素
+     * count > 0: 从头往尾移除值为 value 的元素。
+     * count < 0: 从尾往头移除值为 value 的元素。
+     * count = 0: 移除所有值为 value 的元素。
+     */
+    public Long lrem(String key, long count, Object value) {
+        return listOperations.remove(key, count, value);
+    }
+
+    /**
+     * LSET key index value 设置队列里面一个元素的值
+     */
+    public void lset(String key, long index, Object value) {
+        listOperations.set(key, index, value);
+    }
+
+    /**
+     * LTRIM key start stop 修剪到指定范围内的清单
+     * 如果 start 超过列表尾部，或者 start > end，结果会是列表变成空表（即该 key 会被移除）。
+     * 如果 end 超过列表尾部，Redis 会将其当作列表的最后一个元素。
+     */
+    public void ltrim(String key, long start, long end) {
+        listOperations.trim(key, start, end);
+    }
+
+    /**
+     * RPOP key 从队列的右边出队一个元素
+     */
+    public Object rpop(String key) {
+        return listOperations.rightPop(key);
+    }
+
+    /**
+     * RPUSH key value [value ...] 从队列的右边入队一个元素
+     */
+    public Object rpush(String key, Object value) {
+        return listOperations.rightPush(key, value);
+    }
+
+    /**
+     * RPUSH key value [value ...] 从队列的右边入队多個元素
+     */
+    public Long rpush(String key, Object... values) {
+        return listOperations.rightPushAll(key, values);
+    }
+
+    /**
+     * RPUSH key value [value ...] 从队列的右边入队一个集合
+     */
+    public Long rpush(String key, Collection<Object> values) {
+        return listOperations.rightPushAll(key, values);
+    }
+
+    /**
+     * LPUSHX key value 当队列存在时，从队到左边入队一个元素
+     */
+    public Long rpushx(String key, Object value) {
+        return listOperations.rightPushIfPresent(key, value);
+    }
+
+    /**
+     * HDEL key field [field ...] 删除一个或多个Hash的field
+     */
+
+    /**
+     * HEXISTS key field 判断field是否存在于hash中
+     */
+
+    /**
+     * HGET key field 获取hash中field的值
+     */
+
+    /**
+     * HGETALL key 从hash中读取全部的域和值
+     */
+
+    /**
+     * HINCRBY key field increment 将hash中指定域的值增加给定的数字
+     */
+
+    /**
+     * HINCRBYFLOAT key field increment 将hash中指定域的值增加给定的浮点数
+     */
+
+    /**
+     * HKEYS key 获取hash的所有字段
+     */
+
+    /**
+     * HLEN key 获取hash里所有字段的数量
+     */
+
+    /**
+     * HMGET key field [field ...] 获取hash里面指定字段的值
+     */
+
+    /**
+     * HMSET key field value [field value ...] 设置hash字段值
+     */
+
+    /**
+     * HSET key field value 设置hash里面一个字段的值
+     */
+
+    /**
+     * HSETNX key field value 设置hash的一个字段，只有当这个字段不存在时有效
+     */
+
+    /**
+     * HSTRLEN key field 获取hash里面指定field的长度
+     */
+
+    /**
+     * HVALS key  获得hash的所有值
+     */
+
+    /**
+     * HSCAN key cursor [MATCH pattern] [COUNT count] 迭代hash里面的元素
+     */
+
 
 }
